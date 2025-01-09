0e26940ee01888a20a968166a881a54d1376a58d
premkaur949 <kaurprem909@gmail.com>
2025-01-09 22:13:51 +0530

    .

--- portfolio/src/components/Works/works.js
+++ portfolio/src/components/Works/works.js
@@ -14,7 +14,7 @@ const Works = () => {
         <h2 className='worksTitle'>My Portfolio</h2>
         <span className='worksDesc'>I take pride in my attention to detail, ensuring that every project is an opportunity for me to pixel-perfect my work. Through working on various small projects, I have steadily built my skills and experience to where they are today. I am excited to continue growing and making a positive impact in the industry</span>
         <br></br><div className='worksImgs'>
-            <img src={parallax} alt='Parallax Website Design' className='worksImg' />
+            <img src={parallax} alt='Parallax Website Design' className='worksImg' href="https://premkaur949.netlify.app/"/>
             <img src={Portfolio2} alt='' className='worksImg' />
             <img src={Portfolio3} alt='' className='worksImg' />
             <img src={Portfolio4} alt='' className='worksImg' />
