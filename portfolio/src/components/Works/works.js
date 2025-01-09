8ec6be9c20d1eb7f2ad806be5cbbf7ea639c6f4b
premkaur949 <kaurprem909@gmail.com>
2025-01-09 21:57:39 +0530

    .

--- portfolio/src/components/Works/works.js
+++ portfolio/src/components/Works/works.js
@@ -6,6 +6,7 @@ import Portfolio3 from '../../assets/portfolio-3.png';
 import Portfolio4 from '../../assets/portfolio-4.png';
 import Portfolio5 from '../../assets/portfolio-5.png';
 import Portfolio6 from '../../assets/portfolio-6.png';
+import parallax from '../../assets/parallax.png';
 
 const Works = () => {
   return (
@@ -13,7 +14,7 @@ const Works = () => {
         <h2 className='worksTitle'>My Portfolio</h2>
         <span className='worksDesc'>I take pride in my attention to detail, ensuring that every project is an opportunity for me to pixel-perfect my work. Through working on various small projects, I have steadily built my skills and experience to where they are today. I am excited to continue growing and making a positive impact in the industry</span>
         <br></br><div className='worksImgs'>
-            <img src={Portfolio1} alt='' className='worksImg' />
+            <img src={parallax} alt='Parallax Website Design' className='worksImg' />
             <img src={Portfolio2} alt='' className='worksImg' />
             <img src={Portfolio3} alt='' className='worksImg' />
             <img src={Portfolio4} alt='' className='worksImg' />
