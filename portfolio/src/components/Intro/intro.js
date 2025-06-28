14f035c4fb39b37b63ab3ec5fcc452f91f560370
premkaur949 <kaurprem909@gmail.com>
2025-06-29 00:06:59 +0530

    ..

--- portfolio/src/components/Intro/intro.js
+++ portfolio/src/components/Intro/intro.js
@@ -12,7 +12,7 @@ const Intro = () => {
           <span className='hello'> Hello,</span>
           <span className='introText'>I'm<span className='introName'> Prem Kaur,</span><br/>Website Designer</span>
           <p className='introPara'> I am a skilled web developer with experience in creating<br/> visually appealing websites</p>
-          <a href="https://drive.google.com/file/d/11iROZB1L4cQEDziO3TamDMoyIOcie5zj/view?usp=drive_link" target="_blank" >
+          <a href="https://drive.google.com/file/d/1HsFWCL0mv-bjtZ54TT_dqEa_o0T38imj/view?usp=sharing" target="_blank" >
           <button className='btn'><img src={btnImg} alt='' className='btnImg'/>Resume</button></a>
         </div>
         <img src={bg} alt='Profile' className='bg' />
